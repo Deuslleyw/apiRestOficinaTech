01ba6c3a962056de4051dd2e3a435b227d9c720f
Deuslleyw <Deuslleyw@hotmail.com>
2026-04-29 21:42:42 -0300

    setResourcesDB

--- src/main/java/com/deusleyDev/apiOficina/StarterAPP.java
+++ src/main/java/com/deusleyDev/apiOficina/StarterAPP.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiOficinaApplication {
+public class StarterAPP {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiOficinaApplication.class, args);
+		SpringApplication.run(StarterAPP.class, args);
 	}
 
 }
