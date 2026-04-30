fe69534c7be6f87694d5d1835db8661cb01843ba
Deuslleyw <Deuslleyw@hotmail.com>
2026-04-29 21:15:25 -0300

    firstCommit

---
+++ src/test/java/com/deusleyDev/apiOficina/ApiOficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusleyDev.apiOficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
